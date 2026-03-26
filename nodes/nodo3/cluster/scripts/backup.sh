25120ffc02e8c24062a834e9223b78241274041d
juancarmuoz08 <juan20003080@hotmail.com>
2026-03-26 16:25:11 +0000

    implementando scripts shell

--- nodes/nodo3/cluster/scripts/backup.sh
+++ nodes/nodo3/cluster/scripts/backup.sh
@@ -1,1 +1,11 @@
 # !/bin/bash
+
+echo !========== BACKUP =========="
+echo "Nodo: ${hostname}"
+echo "[INFO] Iniciando backup..."
+
+sleep 1
+echo "[INFO] Copiando archivos..."
+sleep 1
+
+echo "[SUCCESS] Backup completado"
