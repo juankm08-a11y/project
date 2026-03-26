2c015e45e708c6373980b5c9d4da5646f4592a22
juancarmuoz08 <juan20003080@hotmail.com>
2026-03-26 13:31:12 +0000

    creando deploy

---
+++ nodes/nodo1/cluster/scripts/deploy.sh
@@ -0,0 +1,13 @@
+# !/bin/bash
+
+echo !========== DEPLOY =========="
+echo "Nodo: ${hostname}"
+echo "[INFO] Iniciando despliegue..."
+
+for i in {1..3}
+do 
+    echo "[PROGRESS] Paso $i/3..."
+    sleep 1
+done
+
+echo "[SUCCESS] Deploy completado"
